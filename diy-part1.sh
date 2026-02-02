b6256ceb8acff4687bcf1a3963b7230c5bd8b597
BG4TGO-Tom <bg4tgo@126.com>
2026-02-02 14:31:15 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,4 @@
+src-git packages https://git.openwrt.org/feed/packages.git
+src-git luci https://github.com/lazywalker/luci.git
+src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt
+src-git devtools https://github.com/lazywalker/devtools-feeds
